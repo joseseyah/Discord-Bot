ca9ce1e1defa592864fd358980d1994c57ec29e0
Joseph Hayes <joseph.hayes-3@student.manchester.ac.uk>
2022-07-31 10:56:38 +0100

    24hours in seconds added

--- discord.py
+++ discord.py
@@ -4,8 +4,8 @@ import time
 username = input("Please eneter your discord username: ")
 
 while True:
-    time.sleep(10) #time is in seconds
+    time.sleep(86400) #time is in seconds so for 24 hours it would be 86400 seconds
     discord = Discord(
     url="https://discordapp.com/api/webhooks/1002548075375624255/7d78qgrRLwMwo6RFPzB-QvppEWsV07tv_RMUorGqrlNQf6eBQ2mvparDnb4GYfes7smE"
     )
-    discord.post(content =f"@{username}, how are you doing today?")
+    discord.post(content =f"@{username}")
